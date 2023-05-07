ad0bc3229cbb6554d4ed07d674d224eb5aeb2654
nsretkovic <nsretkovic23@gmail.com>
2023-05-07 20:34:23 +0200

    Return more contextual response for Constraint Validation Fail
    - Check neo4j's returned error and look for "ConstraintValidationFailed" string, if it exists, this means that username that user tried to sign up with is already taken

--- app/api/neo4j/users/route.ts
+++ app/api/neo4j/users/route.ts
@@ -1,6 +1,7 @@
 import { User } from "@/libs/interfaces";
 import { session } from "@/utils/neo4j";
 
+// Constraint for unique username: CREATE CONSTRAINT FOR (u:User) REQUIRE u.username IS UNIQUE
 // Creating a User (Sign Up)
 export async function POST(request: Request) {
     let user = await request.json() as User;
@@ -8,8 +9,13 @@ export async function POST(request: Request) {
         // TODO: Handle when user creates a team
         await session.run('CREATE (u:User {username: $username, password: $password})', { username: user.username, password: user.password });
         return new Response(JSON.stringify(user), {status:200});
-
-    } catch(error) {
-        return new Response(JSON.stringify({meessage:"Error creating a new user"}), {status:500})
+    } catch(error: any) {
+        let errorMessage = error.code;
+        // Return specific message only when constraint validation fails which means that user attempted sign up with username that already exists
+        // For everything else, just return error code
+        if(error?.code.includes("ConstraintValidationFailed")) {
+            errorMessage = "Error: Username already taken";
+        }
+        return new Response(JSON.stringify({errorMessage:errorMessage}), {status:400})
     }
 }
