f3af7650fac41ddff26f4036ee5c992e53d913ab
nsretkovic <nsretkovic23@gmail.com>
2023-05-07 20:31:26 +0200

    Try authentication when user enters home page
    - Mock auth: When entering web app's home page, try to get user's credentials if they are stored in localStorage. If they are, try authentication, if they are not or auth failed, redirect user to login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,78 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
-import UserProvider from "@/context/UserContext";
-import Login from "./login/page";
+import { User } from "@/libs/interfaces";
+import { useEffect, useState, useContext, useCallback } from "react";
+import { useRouter } from "next/navigation";
+import { UserContext, UserContextType } from "@/context/UserContext";
+
+function getUserFromLocalStorage(): User | null {
+  const userInfo = localStorage.getItem("loggedInUser");
+
+  if (!userInfo) return null;
+
+  return JSON.parse(userInfo) as User;
+}
 
 export default function Home() {
+  const router = useRouter();
+  //const userInfo = useRef<User | null>(null);
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+  const { user, loginUser } = useContext(UserContext) as UserContextType;
+
+  // Local storage is inaccessible or not defined until component is rendered
+  // So wait in useEffect for component to render and then try to obtain user data
+  useEffect(() => {
+    const userInfoFromLocalStorage = localStorage.getItem("loggedInUser");
+    if (typeof window !== "undefined") {
+      if (userInfoFromLocalStorage) {
+        setUserInfo(JSON.parse(userInfoFromLocalStorage) as User);
+      } else {
+        router.replace("/login");
+      }
+    }
+  }, [router]);
+
+  const fetchUser = useCallback(
+    async (userInfo: User) => {
+      const redirectToLoginPageAndDeleteLocalStorageData = () => {
+        router.replace("/login");
+        localStorage?.removeItem("loggedInUser");
+      };
+
+      if (userInfo) {
+        // Try to fetch user with username and password read from localStorage
+        const response = await fetch(
+          `http://localhost:3000/api/neo4j/users/auth/`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(userInfo),
+          }
+        );
+        // Get response data
+        const data = await response.json();
+
+        // If there is any error, redirect to login page, also delete localStorage data
+        if (data.errorMessage) {
+          console.error(data.errorMessage);
+          redirectToLoginPageAndDeleteLocalStorageData();
+        } else {
+          // If everything is ok, login user(set context)
+          loginUser(data as User);
+        }
+      }
+    },
+    [loginUser, router]
+  );
+
+  // When userInfo is loaded from localStorage, perform user authentication
+  useEffect(() => {
+    if (userInfo) fetchUser(userInfo as User);
+  }, [userInfo, fetchUser]);
+
   return (
     <main className={styles.main}>
-      <h1>Hello nextjs</h1>
+      <h1>Test Auth: {user?.username}</h1>
     </main>
   );
 }
