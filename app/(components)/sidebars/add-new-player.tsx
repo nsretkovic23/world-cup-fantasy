7bea38188688124d60b71a723fc5ca9af51b4f37
nsretkovic23 <nsretkovic23@gmail.com>
2023-05-29 00:06:56 +0200

    Create new empty component for adding the player to the team

---
+++ app/(components)/sidebars/add-new-player.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './page.module.css';
+import { Star } from '@mui/icons-material';
+import { orange, pink, yellow } from '@mui/material/colors';
+
+function AddNewPlayer() {
+  return (
+    <div className={styles.sidebarContainer}>
+    </div>
+  )
+}
+
+export default AddNewPlayer;
