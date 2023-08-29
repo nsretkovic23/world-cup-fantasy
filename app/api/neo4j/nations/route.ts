498c9d92dc3456b1d6417d27016dab47f2e184d2
nsretkovic23 <nsretkovic23@gmail.com>
2023-08-29 23:53:53 +0200

    Implement patch method for adding relation :HAS_PLAYER towards player

--- app/api/neo4j/nations/route.ts
+++ app/api/neo4j/nations/route.ts
@@ -1,8 +1,9 @@
-import { Nation } from "@/lib/interfaces/db-data-Interfaces";
+import { Nation, Player } from "@/lib/interfaces/db-data-Interfaces";
 import { session } from "@/lib/databases/neo4j";
 import { NextApiRequest } from "next";
 import { QueryResult } from "neo4j-driver";
 
+// Getting all nations
 export async function GET(request: NextApiRequest, { params }:{params:any}) {
     try {
     let result: QueryResult = await session.run("MATCH (n:Nation) RETURN n");
@@ -20,3 +21,34 @@ export async function GET(request: NextApiRequest, { params }:{params:any}) {
         return new Response(JSON.stringify({errorMessage:errorMessage}), {status:500});
     }
 }
+
+// One-time updating nation and every player from this nation with another relation from nation towards player (:HAS_PLAYER)
+// This function does not need to be used anymore that's why it's commented
+// When creating new player, this relation will automatically be created
+// At the beginning there was only player - to - nation relation :PLAYS_FOR, but, for example, filtering players by nation,
+// Relation from nation - to - player would be more efficient
+
+// export async function PATCH(request:Request) {
+//     let req = await request.json();
+//     let query = `MATCH(n:Nation {name:"${req.nationName}"}),`;
+    
+//     req.players.forEach((player:Player, index:number) => {
+//         query += `\n(p${index}:Player {name:"${player.name}"})` 
+        
+//         if(index !== req.players.length - 1) {
+//             query += ",";
+//         }
+//     });
+
+//     for(let i = 0; i < req.players.length; i++) {
+//         // Merge ensures that only one relation is created, in case that there is already one existing :HAS_PLAYER relation
+//         query += `\nMERGE (n)-[:HAS_PLAYER]->(p${i})`; 
+//     }
+
+//     try{
+//         await session.run(query);
+//         return new Response(JSON.stringify(query), {status:200});
+//     } catch(error:any) {
+//         return new Response(JSON.stringify({errorMessage:error}), {status:400});
+//     }
+// }
