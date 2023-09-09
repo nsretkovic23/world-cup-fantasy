47deeb5e6fdf53a5382ca4936f6ce9ee7ef040bb
nsretkovic23 <nsretkovic23@gmail.com>
2023-09-09 22:56:48 +0200

    Install and setup redis

--- lib/env.ts
+++ lib/env.ts
@@ -3,3 +3,5 @@
 export const NEO4J_URI="neo4j+s://ee4185ba.databases.neo4j.io";
 export const NEO4J_USERNAME= "neo4j";
 export const NEO4J_PASSWORD= "r0dxumLZtab42nJpBs6s0up8mnbqa8Q6HfN1z-AT4ds";
+// Redis is with two s because encrypted trafic is enabled
+export const REDIS_URL = "rediss://default:bfa39a5c4bc74218bb3ef35016ec4e58@eu1-good-sunbeam-39855.upstash.io:39855";
